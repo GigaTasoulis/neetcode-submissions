b4b4fb3dd3e0f1179a9ac79874b82dcd0d9638ea
Anastasios Giannakakis <119429929+GigaTasoulis@users.noreply.github.com>
2026-07-31 12:46:05 +0300

    Add: top-k-elements-in-list - submission-5

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-5.js
@@ -0,0 +1,27 @@
+class Solution {
+    topKFrequent(nums, k) {
+        const frequencies = new Map();
+
+        for (const num of nums){
+            const currentFrequency = frequencies.get(num) ?? 0;
+            frequencies.set(num, currentFrequency + 1);
+        }
+
+        const entries = [...frequencies]; 
+
+        // each entry is an inner array [1, 1] vs [2, 2]
+        entries.sort((a,b) => b[1] - a[1])
+
+        const results = [];
+        
+        for (const [num] of entries){
+            results.push(num);
+
+            if (results.length === k){
+                break;
+            }
+        }
+        
+        return results;
+    }
+}
