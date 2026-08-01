aa90e5a6d81af2fe4e75f7e9a056dc27e1ebc70c
Anastasios Giannakakis <119429929+GigaTasoulis@users.noreply.github.com>
2026-08-01 19:31:28 +0300

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let prefix = 1;
+        let suffix = 1;
+        let right = [];
+        let left = [];
+        let result =[];
+
+        for (let i=0; i < nums.length; i++){
+            result[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        for (let i = nums.length - 1; i >= 0; i--){
+            result[i] *= suffix;
+            suffix *= nums[i];
+
+        }
+
+        return result;
+    }
+}
